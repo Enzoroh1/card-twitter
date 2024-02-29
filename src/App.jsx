e412ccc1878839a04e359c1e3540f00f07d5491a
Enzoroh1 <enzoroh1@hotmail.com>
2024-02-29 20:46:47 -0300

    Comienzo

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+export function App () {
+  return (
+    <article>
+      <header>
+        <img src="https://enzodev.com.ar/static/media/enzo1.f209b90d786e9d66d78e.jpg" alt="" />
+        <div>
+          <strong>Enzo Roh</strong>
+          <span>@enzodev</span>
+        </div>
+        {/* <img src="https://www.cariverplate.com.ar/images/logo-river.png?cache=a57" alt="" /> */}
+        {/* <img src="https://imgs.search.brave.com/eEbtvI8cqaQ9yBQR5jUA8401hlvtQhn83hu0UIYoIoQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbnRl/cmlvcmZ1dGJvbGVy/by5jb20uYXIvd3At/Y29udGVudC91cGxv/YWRzLzIwMjEvMTIv/c2FuLWpvcmdlLXZp/bGxhLWVsaXNhLTEt/MS5wbmc" alt="" /> */}
+      </header>
+      <aside>
+        <button>
+          Seguir
+        </button>
+      </aside>
+    </article>
+  )
+}
